8641bde4c28c4c68c899bc53a62ae224d6b47332
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-09-24 13:45:02 +0900

    added reducers

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 
+import Flat from "../components/flat";
+
 class FlatList extends Component {
-  render() {
-    return (
-      <div className="col-sm-7">
-        TODO
-      </div>
-    );
-  };
+  static defaultProps = { 
+    flats: [{ 
+      "name": "Charm at the Steps of Montmartre", 
+      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+      "price": 164,
+      "priceCurrency": "EUR"
+    }] 
+  } 
+
+  componentWillMount() {
+    // TODO: dispatchan action to loat flats
+  }
+
+  render() { 
+    return ( 
+      <div className="flat-list col-sm-7"> 
+        {this.props.flats.map((flat) => <Flat flat={flat} key={flat.name} />)}
+      </div> 
+    ); 
+  }
 }
 
 export default FlatList;
---
+++ src/reducers/flat_reducer.js
@@ -0,0 +1,10 @@
+const flatsReducer = (state, action) => {
+  if (state === undefined) {
+    // Reducer initialization
+    return [];
+  }
+
+  // todo: handle some actions
+};
+
+export default flatsReducer;
